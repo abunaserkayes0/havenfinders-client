e310ef4f8009204f2a2323c43c5c3e67e22ac2db
abunaserkayes0 <20234203080@cse.bubt.edu.bd>
2025-01-08 08:52:48 +0600

    init:Project-Structure

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { webfontDownload } from 'vite-plugin-webfont-dl'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    webfontDownload([
+      "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+    ])
+  ],
+})
